f96949668aa305e36608e33d3cec9d753bf9f15d
Brandon Goding <bgoding@hometap.com>
2025-06-19 21:47:18 -0400

    refactor(footer.js): update github link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="social-links">
                     <a href="https://www.facebook.com/FortKentCinema/" target="_blank" rel="noopener noreferrer">Facebook</a>
                     <a href="https://www.instagram.com/fortkentcinema/" target="_blank" rel="noopener noreferrer">Instagram</a>
-                    <a href="https://github.com/BrandonGoding/fortkent_cinema" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="https://github.com/BrandonGoding/www_fortkentcinema_com" target="_blank" rel="noopener noreferrer">GitHub</a>
                     <a href="https://www.youtube.com/@FortKentCinema" target="_blank" rel="noopener noreferrer">YouTube</a>
                 </div>
             </div>
