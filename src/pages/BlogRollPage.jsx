a90bc2223f7b1f0d85b3eeb1a8260a43e40d621e
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-06-29 13:14:13 -0400

    refactor(constants): update default constant

--- src/pages/BlogRollPage.jsx
+++ src/pages/BlogRollPage.jsx
@@ -57,7 +57,7 @@ const BlogRollPage = () => {
     ? (() => {
         const decoded = decodeURIComponent(post.header_image);
         const trimmed = decoded.startsWith("/") ? decoded.slice(1) : decoded;
-        return trimmed.replace("%3A", "/").replace("%2F", "/");
+        return trimmed.replace("https:%3A/", "https://").replace("%2F", "/");
       })()
     : placeHolder
 }
