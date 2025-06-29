a0d2dcbc9666ef9496f8b53874c2210dbb07c426
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-06-29 11:57:21 -0400

    refactor(constants): update default constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api.fortkentcinema.com";
 const BLOGROLL_URL = `${API_BASE_URL}/api/v1/blog/`;
 const BLOG_BY_SLUG_URL = `${API_BASE_URL}/api/v1/blog/`;
 const NOW_PLAYING_URL = `${API_BASE_URL}/api/v1/films/now-playing/`;
