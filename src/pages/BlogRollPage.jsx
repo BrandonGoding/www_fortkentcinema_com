dbcec7e24481b62f4e1ac4a71a5bc13d0c999798
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-06-29 18:47:14 -0400

    refactor(header_image): gets url from s3

--- src/pages/BlogRollPage.jsx
+++ src/pages/BlogRollPage.jsx
@@ -53,10 +53,14 @@ const BlogRollPage = () => {
                   <img
                     alt=""
                     src={
-  post.header_image
-    ? post.header_image
-    : placeHolder
-}
+                      post.header_image
+                        ? (() => {
+                            const decoded = decodeURIComponent(post.header_image);
+                            const cleaned = decoded.replace("https%3A/cdn.fortkentcinema.com/", "");
+                            return `https://cdn.fortkentcinema.com/${cleaned}`;
+                          })()
+                        : placeHolder
+                    }
                     className="aspect-video w-full rounded-2xl bg-gray-100 object-cover sm:aspect-2/1 lg:aspect-3/2"
                   />
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-gray-900/10 ring-inset" />
