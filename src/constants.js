4389bb55b87bc98d099fba2a4fee7fd9f4ce0916
Brandon Goding <brandon.h.goding@gmail.com>
2025-07-05 22:25:56 -0400

    refactor(constants): update a;;_films_url

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api.fortkentc
 const BLOGROLL_URL = `${API_BASE_URL}/api/v1/blog/`;
 const BLOG_BY_SLUG_URL = `${API_BASE_URL}/api/v1/blog/`;
 const NOW_PLAYING_URL = `${API_BASE_URL}/api/v1/films/now-playing/`;
-const ALL_FILMS_URL = `${API_BASE_URL}/api/v1/films/`;
+const ALL_FILMS_URL = `${API_BASE_URL}/api/v1/films/archive/`;
 const COMING_SOON_URL = `${API_BASE_URL}/api/v1/films/coming-soon/`;
 
 const COMING_SOON_CALENDAR_URL = `${API_BASE_URL}/api/v1/films/coming-soon/calendar/`;
